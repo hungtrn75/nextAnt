f5899a358663440197344f5a04b51fe1e285106c
Max Li <maxlivinci@gmail.com>
2018-05-14 14:59:48 +0800

    add .travis.yml

--- server.js
+++ server.js
@@ -1,42 +1,39 @@
-var express = require("express");
-var next = require("next");
-var {
+//const { IntlMiddle } =require("./serverMiddle/IntlMiddle")
+const express = require('express')
+const next = require('next')
+const accepts = require('accepts')
+const { parse } = require('url')
+const glob = require('glob')
+const { basename } = require('path')
+const { readFileSync } = require('fs')
+const cors = require('cors')
+
+const {
   ApolloMiddleInterFace,
   ApolloMiddle
-} = require("./serverMiddle/apolloMiddle");
-var schema = require("./backend");
-//var { IntlMiddle } =require("./serverMiddle/IntlMiddle")
-//多國語系
-var accepts = require("accepts");
-var { parse } = require("url");
-var glob = require("glob");
-var { basename } = require("path");
-var { readFileSync } = require("fs");
-var cors = require("cors");
+} = require('./serverMiddle/apolloMiddle')
+const schema = require('./backend')
 
-var port = process.env.NODE_ENV !== "production" ? 3000 : 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
-//console.log(AllSchema)
+const port = process.env.PORT || 3000
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
 app.prepare().then(() => {
-  const server = express();
-  server.use(cors());
+  const server = express()
+  server.use(cors())
 
-  ApolloMiddle(server, schema);
-  ApolloMiddleInterFace(server);
+  ApolloMiddle(server, schema)
+  ApolloMiddleInterFace(server)
 
-  server.get("*", (req, res) => {
-    // console.log('req.messages')
-    // console.log(req.messages)
-    return handle(req, res);
-  });
+  server.get('*', (req, res) => handle(req, res))
+
   server.listen(port, err => {
-    if (err) throw err;
-    //	console.log(`> xxx ${process.env.NODE_ENV}`);
-    console.log(`> ready  on http://localhost:${port}`);
-  });
-});
+    if (err) throw err
+    console.log(`> ready  on http://localhost:${port}`)
+  })
+})
 
 // const getMessages = (locale) => {
 //   return require(`./lang/${locale}.json`)
