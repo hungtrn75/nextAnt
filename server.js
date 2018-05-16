048c5d62edd12414fac33ccd5f1628265a09d87c
Max Li <maxlivinci@gmail.com>
2018-05-16 09:55:32 +0800

    add mongodb

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-//const { IntlMiddle } =require("./serverMiddle/IntlMiddle")
 const express = require('express')
+const mongoose = require('mongoose')
 const next = require('next')
-const accepts = require('accepts')
-const { parse } = require('url')
-const glob = require('glob')
-const { basename } = require('path')
-const { readFileSync } = require('fs')
 const cors = require('cors')
 
-const {
-  ApolloMiddleInterFace,
-  ApolloMiddle
-} = require('./serverMiddle/apolloMiddle')
+require('./backend/models/customer')
 const schema = require('./backend')
+const {
+  apolloMiddleInterFace,
+  apolloMiddle
+} = require('./serverMiddle/apolloMiddle')
+const keys = require('./config/keys')
+
+mongoose.Promise = global.Promise
+mongoose.connect(keys.mongoURI)
 
 const port = process.env.PORT || 3000
-
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -24,47 +23,13 @@ app.prepare().then(() => {
   const server = express()
   server.use(cors())
 
-  ApolloMiddle(server, schema)
-  ApolloMiddleInterFace(server)
+  apolloMiddle(server, schema)
+  apolloMiddleInterFace(server)
 
   server.get('*', (req, res) => handle(req, res))
 
   server.listen(port, err => {
     if (err) throw err
-    console.log(`> ready  on http://localhost:${port}`)
+    console.log(`> ready on http://localhost:${port}`)
   })
 })
-
-// const getMessages = (locale) => {
-//   return require(`./lang/${locale}.json`)
-// }
-// const languages = glob.sync('./lang/*.json').map((f) => basename(f, '.json'))
-
-// const localeDataCache = new Map()
-
-// const getLocaleDataScript = (locale) => {
-//   const lang = locale.split('-')[0]
-//   if (!localeDataCache.has(lang)) {
-//     const localeDataFile = require.resolve(`react-intl/locale-data/${lang}`)
-//     const localeDataScript = readFileSync(localeDataFile, 'utf8')
-//     localeDataCache.set(lang, localeDataScript)
-//   }
-//   return localeDataCache.get(lang)
-// }
-// server.use((req, res, next) => {
-//   const parsedUrl = parse(req.url, true)
-//   const accept = accepts(req)
-//   console.log('=====req.query====')
-//   // console.log(parsedUrl)
-//   //這邊會抓取header去判斷網頁客戶端的類別
-//   let locale = accept.language(languages)
-//   locale = (req.query.locale) ? req.query.locale : locale ? locale : 'en'
-//   //locale = locale ? locale : 'en'
-//   // console.log('locale')
-//   // console.log(locale)
-//   req.locale = locale
-//   //req.localeDataScript = getLocaleDataScript(locale)
-//   req.messages = getMessages(locale)
-
-//   next()
-// })
--- serverMiddle/apolloMiddle.js
+++ serverMiddle/apolloMiddle.js
@@ -1,25 +1,27 @@
-var { graphqlExpress, graphiqlExpress } = require("apollo-server-express")
-var bodyParser = require("body-parser")
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const bodyParser = require('body-parser')
 
-
-const ApolloMiddle = (server, schema) => server.use(
-  "/graphql",
-  bodyParser.json(),
-  graphqlExpress(async () => {
-    return {
-      schema,
-      // context: { user: req.user },
-      // tracing: true,
-      // cacheControl: true
-    };
-  })
-);
-const ApolloMiddleInterFace = (server) => {
+const apolloMiddle = (server, schema) =>
   server.use(
-    "/graphiql",
-    graphiqlExpress({
-      endpointURL: "/graphql",
+    '/graphql',
+    bodyParser.json(),
+    graphqlExpress(async () => {
+      return {
+        schema
+        // context: { user: req.user },
+        // tracing: true,
+        // cacheControl: true
+      }
     })
   )
-};
-module.exports = { ApolloMiddle, ApolloMiddleInterFace }
+
+const apolloMiddleInterFace = server => {
+  server.use(
+    '/graphiql',
+    graphiqlExpress({
+      endpointURL: '/graphql'
+    })
+  )
+}
+
+module.exports = { apolloMiddle, apolloMiddleInterFace }
