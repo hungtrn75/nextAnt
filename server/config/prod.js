db6920f3255b1fad03f8be2fadcd18a2a9d747ca
Max Li <maxlivinci@gmail.com>
2018-05-23 10:12:09 +0800

    fix: production cors error

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  origin: 'https://next-ant.herokuapp.com/',
+  origin: 'https://next-ant.herokuapp.com',
   mongoURI: 'mongodb://admin:password@ds133657.mlab.com:33657/next-ant',
   cookieKey: 'asdasdklh2wdasd28ssS',
   secret: 'wdasd28sSGW32s&'
