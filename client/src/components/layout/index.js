d987316e5b64824697b27bcd816fee314cbae8a9
線上讀書會 <polo13999@gmail.com>
2018-05-23 14:06:02 +0800

    feat: add react scope

--- client/src/components/layout/index.js
+++ client/src/components/layout/index.js
@@ -1,99 +1,68 @@
 import React from 'react'
 import { Avatar, Layout, Icon, Row, Col } from 'antd'
-import { adopt } from 'react-adopt'
-import { Toggle, Value, State } from 'react-powerplug'
 
 import Nav from '../nav'
+
 const { Header, Content, Sider } = Layout
 
-const AdoptContainer = adopt({
-  toggleMenuModal: <Toggle initial={false} />,
-  token: <Value initial={''} />,
-  loginState: <State initial={{ loggedIn: false }} />
-})
-export const GlobalBlock = React.createContext()
-
-export default props => (
-  <AdoptContainer>
-    {({ toggleMenuModal, loginState }) => {
-      let loginblock = {}
-
-      const handleLogout = resultX => {
-        resultX.e.preventDefault()
-        loginState.setState({ loggedIn: false })
-      }
-
-      const handleLogin = resultX => {
-        resultX.e.preventDefault()
-        resultX.form.validateFields(async (err, values) => {
-          if (!err) {
-            resultX.loginAction.mutation({ variables: values })
-            loginState.setState({ loggedIn: true })
-          }
-        })
-      }
-
-      const handleLoginEvent = { handleLogout, handleLogin }
-
-      loginblock = { loginState, handleLoginEvent }
-
-      return (
-        <GlobalBlock.Provider value={loginblock}>
-          <Layout>
-            <Sider
-              trigger={null}
-              collapsible
-              collapsed={toggleMenuModal.on}
-              style={{ minHeight: '100vh' }}
-              breakpoint="lg"
-              collapsedWidth="0"
-            >
-              <div className="logo" />
-              <Nav />
-            </Sider>
-            <Layout>
-              <Header style={{ background: '#fff', padding: 0 }}>
-                <Row type="flex" justify="space-between">
-                  <Col span={1}>
-                    {' '}
-                    <Icon
-                      className="trigger"
-                      type={toggleMenuModal.on ? 'menu-unfold' : 'menu-fold'}
-                      onClick={toggleMenuModal.toggle}
-                    />
-                  </Col>
-                  <Col span={20}>
-                    <div
-                      style={{
-                        position: 'absolute',
-                        top: '-5px',
-                        right: '20px'
-                      }}
-                    >
-                      <span style={{ marginRight: '20px;' }}>Main</span>
-                      <Icon type="bell" style={{ marginRight: '20px' }} />
-                      <Avatar
-                        style={{ backgroundColor: '#87d068' }}
-                        icon="user"
-                      />
-                    </div>
-                  </Col>
-                </Row>
-              </Header>
-              <Content
-                style={{
-                  margin: '24px 16px',
-                  padding: 24,
-                  background: '#fff',
-                  minHeight: 300
-                }}
-              >
-                {props.children}
-              </Content>
-            </Layout>
-          </Layout>
-        </GlobalBlock.Provider>
-      )
-    }}
-  </AdoptContainer>
-)
+export default class SiderDemo extends React.Component {
+  state = {
+    collapsed: false
+  }
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    })
+  }
+  render() {
+    return (
+      <Layout>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+          style={{ minHeight: '100vh' }}
+          breakpoint="lg"
+          collapsedWidth="0"
+        >
+          <div className="logo" />
+          <Nav />
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Row type="flex" justify="space-between">
+              <Col span={1}>
+                {' '}
+                <Icon
+                  className="trigger"
+                  type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                  onClick={this.toggle}
+                  style={{ marginLeft: '15px' }}
+                />
+              </Col>
+              <Col span={20}>
+                <div
+                  style={{ position: 'absolute', top: '-5px', right: '20px' }}
+                >
+                  <span style={{ marginRight: '20px;' }}>Main</span>
+                  <Icon type="bell" style={{ marginRight: '20px' }} />
+                  <Avatar style={{ backgroundColor: '#87d068' }} icon="user" />
+                </div>
+              </Col>
+            </Row>
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              background: '#fff',
+              minHeight: 300
+            }}
+          >
+            {this.props.children}
+          </Content>
+        </Layout>
+      </Layout>
+    )
+  }
+}
