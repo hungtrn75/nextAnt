291723a6fe8dc782215411bfb7bd05a89027ff15
Max Li <maxlivinci@gmail.com>
2018-06-18 17:30:24 +0800

    add google analytics

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -7,6 +7,7 @@ import Layout from '../src/components/layout'
 
 import withApollo from '../src/lib/withApollo'
 import checkLoggedIn from '../src/lib/checkLoggedIn'
+import { initGA, logPageView } from '../src/lib/analytics'
 
 class MyApp extends App {
   static async getInitialProps(ctx, apolloClient) {
@@ -16,15 +17,37 @@ class MyApp extends App {
     }
   }
 
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    logPageView()
+  }
+
   render() {
-    const { Component, pageProps, loginUser } = this.props
+    const { Component, pageProps, loginUser, styleTags } = this.props
 
     return (
       <Container>
         <Head>
+          <title>Next Ant</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
-          <link rel="stylesheet" href="/_next/static/style.css" />
+          <link rel="icon" href="static/img/favicon.ico" type="image/x-icon" />
+          <link
+            rel="icon"
+            type="image/png"
+            href="static/img/favicon-32x32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="static/img/favicon-16x16.png"
+            sizes="16x16"
+          />
+          {styleTags}
         </Head>
         <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
         <Layout loginUser={loginUser}>
